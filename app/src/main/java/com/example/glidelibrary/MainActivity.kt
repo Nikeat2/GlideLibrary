ae557bfad15d90eeaa0f428b788602bdad279923
Lobzev Nikita <lobzev.n@yandex.ru>
2024-06-01 22:16:33 +0300

    Glide library

--- app/src/main/java/com/example/glidelibrary/MainActivity.kt
+++ app/src/main/java/com/example/glidelibrary/MainActivity.kt
@@ -1,20 +1,31 @@
 package com.example.glidelibrary
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btn: Button = findViewById(R.id.btn)
+        val ivPhoto: ImageView = findViewById(R.id.imageView)
+
+        btn.setOnClickListener {
+            val url = "https://assets.bwbx.io/images/users/iqjWHBFdfxIU/ipwI8jWqfiUQ/v1/-1x-1.jpg"
+
+            Glide.with(this)
+                .load(url)
+                .into(ivPhoto)
+
+        }
         }
     }
-}
